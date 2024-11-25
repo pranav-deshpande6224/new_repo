17f84e2bfe189f78e6ad0cdd6611c20d8420d96e
pranav-deshpande6224 <deshpande6224@gmail.com>
2024-11-25 21:10:49 +0530

    lastest_code

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "209702042"
+    serviceWorkerVersion: "2713581138"
   }
 });
